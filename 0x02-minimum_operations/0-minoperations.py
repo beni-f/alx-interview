55d2b30ccc4d5e2727535ac7a15345c25ea8b5cd
beni-f <benifissha@gmail.com>
2024-06-16 15:45:05 +0300

    updated

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,16 +2,18 @@
 """
     Minimum Operations
 """
+
+
 def minOperations(n):
     operations = 1
     count = 0
-    while(operations * 2 < n):
+    while (operations * 2 < n):
         count += 1
         operations *= 2
-    if(operations * 2 == n):
+    if (operations * 2 == n):
         count += 1
     else:
-        while(operations != n):
-            count += 1 
+        while (operations != n):
+            count += 1
             operations += 1
     return count
