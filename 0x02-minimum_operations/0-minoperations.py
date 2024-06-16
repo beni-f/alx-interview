58bf20b91f2b491e5d38709f398ade5a4b013d71
beni-f <benifissha@gmail.com>
2024-06-16 17:02:23 +0300

    updated

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,19 +2,17 @@
 """
     Minimum Operations
 """
-
-
 def minOperations(n):
-    if n == 1:
+    if (n <= 0):
         return 0
-
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
-    j = 1
-
-    for i in range(2, n + 1):
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + 1 + (i // j - 1))
-
-    return dp[n] if dp[n] != float('inf') else 0
+    
+    operations = 0
+    iterator = 2
+    while (iterator <= n):
+        if (n % iterator == 0):
+            n = int(n / iterator)
+            operations += iterator
+            iterator = 1
+        iterator += 1
+    return operations
+    
