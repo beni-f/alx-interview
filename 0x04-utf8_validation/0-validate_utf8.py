ec895c31a08c466b2a2cfa68ce2f11b3d07f33cb
beni-f <benifissha@gmail.com>
2024-06-26 21:50:50 +0300

    0-validate_utf8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    UTF-8 Validation
+"""
+
+
+def validUTF8(data):
+    """
+        Determines if a given data set represents a valid UTF-8 encoding.
+    """
+    pass
