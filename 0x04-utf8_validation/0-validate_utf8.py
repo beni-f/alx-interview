f8f92582d8094295c97b633a1dd248639651da74
beni-f <benifissha@gmail.com>
2024-06-27 15:44:58 +0300

    0-validate_utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,4 +8,24 @@ def validUTF8(data):
     """
         Determines if a given data set represents a valid UTF-8 encoding.
     """
-    pass
+
+    n_bytes = 0
+
+    for num in data:
+        byte = num & 0xFF
+
+        if n_bytes == 0:
+            if (byte >> 5) == 0b110:
+                n_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                n_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                n_bytes = 3
+            elif (byte >> 7) != 0:
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            n_bytes -= 1
+
+    return n_bytes == 0
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,4 @@
+## UTF-8 Validation
+
+# Overview
+In this project I've used knowledge in bitwise operations, understanding of the UTF-8 encoding scheme, and Python programming skills to validate whether a given dataset represents a valid UTF-8 encoding.
