d82538bbffaf8ec5f7cf7f969f88eec6ca5422ba
beni-f <benifissha@gmail.com>
2024-07-24 21:37:24 +0300

    0-making_change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+0-making_change
+"""
+def makeChange(coins, total):
+    """
+        Determine the fewest number of coins needed to meet a given amout total
+    """
+    if total <= 0:
+        return 0
+    
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] <= total else -1
